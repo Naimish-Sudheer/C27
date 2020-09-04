205e724f078e77c8d4d0c975db13d7f20a76962f
Naimish <naimishsudheer@gmail.com>
2020-09-04 15:49:23 +0530

    chain

--- sketch.js
+++ sketch.js
@@ -35,7 +35,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    log6 = new Log(160,100,80,PI);
+    chain = new Chain(bird.body,log6.body)
 }
 
 function draw(){
@@ -58,7 +59,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    log6.display();
+    
     bird.display();
     platform.display();
+    chain.display();
 }
